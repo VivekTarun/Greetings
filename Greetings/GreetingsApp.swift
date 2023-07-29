68a107f63a8921521120b30733c17e461d5efa9c
vivektarun <vivektarun1234@gmail.com>
2023-07-29 23:10:59 +0530

    initial file added

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Vivek Tarun on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
